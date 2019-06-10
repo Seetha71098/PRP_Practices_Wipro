8f45b4391ef1e2903e3dd982b170861e6302d88e
Seetha71098 <42378980+Seetha71098@users.noreply.github.com>
2019-06-10 14:51:05 +0530

    Create FancyNumber.java

---
+++ FancyNumber.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+public class FancyNumber {
+	public static boolean cons(String x){
+	char c[]=x.toCharArray();
+	boolean flag=false;
+	for(int i=0;i<c.length-1;i++){
+	if(c[i]==c[i+1] && !(i==c.length-2)){
+	if(c[i+1]==c[i+2]){
+	flag=true;
+	}
+	}
+
+	}
+	return flag;
+
+	}
+	public static boolean asdes(String x){
+	char c[]=x.toCharArray();
+	boolean flag=false;
+	for(int i=0;i<c.length-1;i++){
+	if(c[i]==(c[i+1]+1) && !(i==c.length-2)){
+	if(c[i+1]==(c[i+2]+1)){
+	flag=true;
+	}
+	}
+	if(c[i]==(c[i+1]-1)){
+	if(c[i+1]==(c[i+2]-1)){
+	flag=true;
+	}
+	}
+
+
+	}
+	return flag;
+	}
+	public static boolean rep(String x){
+	char c[]=x.toCharArray();
+	boolean flag=false;
+	int count=0;
+	for(int i=0;i<c.length;i++){
+	count=0;
+	for(int j=0;j<c.length;j++){
+	if(c[i]==c[j]){
+	count++;
+	}
+	}
+	if(count==4){
+	flag=true;
+	break;
+	}
+
+	}
+	return flag;
+
+	}
+
+	public static void main(String args[]){
+	Scanner scan=new Scanner(System.in);
+	String num=scan.next();
+	boolean val1=true,val2=true,val3=true;
+	if(num.length()==10){
+	if(FancyNumber.cons(num)){
+	System.out.println("Your number is Fancy,it satisfies the 1st condition");
+
+	}
+	else{
+	val1=false;
+	}
+	if(FancyNumber.asdes(num)){
+	System.out.println("Your number is Fancy,it satisfies the 2st condition");
+	}
+	else{
+	val2=false;
+	}
+	if(FancyNumber.rep(num)){
+	System.out.println("Your number is Fancy,it satisfies the 3st condition");
+	}
+	else{
+	val3=false;
+	}
+	}
+	else{
+	System.out.println("Invalid Phone number");
+	}
+	if(val1&&val2&&val3){
+	System.out.println("Your number is not fancy");
+	}
+
+
+	}
+
+	}
